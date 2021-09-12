2a005bac058b8ca7ff443139b254cf8c9aa7b5c6
nkg3479 <neerajkg0320@gmail.com>
2021-09-12 18:37:32 +0530

    pop up added

---
+++ js/popup.js
@@ -0,0 +1,130 @@
+var popup = document.getElementById("myPopup");
+var pop1 = document.getElementById("myPopup1");
+var pop2 = document.getElementById("myPopup2");
+var pop3 = document.getElementById("myPopup3");
+var pop4 = document.getElementById("myPopup4");
+var pop5 = document.getElementById("myPopup5");
+var pop6 = document.getElementById("myPopup6");
+var pop7 = document.getElementById("myPopup7");
+var pop8 = document.getElementById("myPopup8");
+var pop9 = document.getElementById("myPopup9");
+function myFunction() {
+    popup.classList.toggle("show");
+    pop1.classList.remove("show");
+    pop2.classList.remove("show");
+    pop3.classList.remove("show");
+    pop4.classList.remove("show");
+    pop5.classList.remove("show");
+    pop6.classList.remove("show");
+    pop7.classList.remove("show");
+    pop8.classList.remove("show");
+    pop9.classList.remove("show");
+  }
+  function myFunction1() {
+    pop1.classList.toggle("show");
+    popup.classList.remove("show");
+    pop3.classList.remove("show");
+    pop2.classList.remove("show");
+    pop4.classList.remove("show");
+    pop5.classList.remove("show");
+    pop6.classList.remove("show");
+    pop7.classList.remove("show");
+    pop8.classList.remove("show");
+    pop9.classList.remove("show");
+  }
+  function myFunction2() {
+    pop2.classList.toggle("show");
+    pop3.classList.remove("show");
+    popup.classList.remove("show");
+    pop1.classList.remove("show");
+    pop4.classList.remove("show");
+    pop5.classList.remove("show");
+    pop6.classList.remove("show");
+    pop7.classList.remove("show");
+    pop8.classList.remove("show");
+    pop9.classList.remove("show");
+  }
+  function myFunction3(){
+    pop3.classList.toggle("show");
+    popup.classList.remove("show");
+    pop1.classList.remove("show");
+    pop2.classList.remove("show");
+    pop4.classList.remove("show");
+    pop5.classList.remove("show");
+    pop6.classList.remove("show");
+    pop7.classList.remove("show");
+    pop8.classList.remove("show");
+    pop9.classList.remove("show");
+  }
+  function myFunction4(){
+    pop4.classList.toggle("show");
+    popup.classList.remove("show");
+    pop1.classList.remove("show");
+    pop2.classList.remove("show");
+    pop3.classList.remove("show");
+    pop5.classList.remove("show");
+    pop6.classList.remove("show");
+    pop7.classList.remove("show");
+    pop8.classList.remove("show");
+    pop9.classList.remove("show");
+  }
+  function myFunction5(){
+    pop5.classList.toggle("show");
+    popup.classList.remove("show");
+    pop1.classList.remove("show");
+    pop2.classList.remove("show");
+    pop3.classList.remove("show");
+    pop4.classList.remove("show");
+    pop6.classList.remove("show");
+    pop7.classList.remove("show");
+    pop8.classList.remove("show");
+    pop9.classList.remove("show");
+  }
+  function myFunction6(){
+    pop6.classList.toggle("show");
+    popup.classList.remove("show");
+    pop1.classList.remove("show");
+    pop2.classList.remove("show");
+    pop3.classList.remove("show");
+    pop4.classList.remove("show");
+    pop5.classList.remove("show");
+    pop7.classList.remove("show");
+    pop8.classList.remove("show");
+    pop9.classList.remove("show");
+  }
+  function myFunction7(){
+    pop7.classList.toggle("show");
+    popup.classList.remove("show");
+    pop1.classList.remove("show");
+    pop2.classList.remove("show");
+    pop3.classList.remove("show");
+    pop4.classList.remove("show");
+    pop5.classList.remove("show");
+    pop6.classList.remove("show");
+    pop8.classList.remove("show");
+    pop9.classList.remove("show");
+  }
+  function myFunction8(){
+    pop8.classList.toggle("show");
+    popup.classList.remove("show");
+    pop1.classList.remove("show");
+    pop2.classList.remove("show");
+    pop3.classList.remove("show");
+    pop4.classList.remove("show");
+    pop5.classList.remove("show");
+    pop6.classList.remove("show");
+    pop7.classList.remove("show");
+    pop9.classList.remove("show");
+  }
+  function myFunction9(){
+    pop9.classList.toggle("show");
+    popup.classList.remove("show");
+    pop1.classList.remove("show");
+    pop2.classList.remove("show");
+    pop3.classList.remove("show");
+    pop4.classList.remove("show");
+    pop5.classList.remove("show");
+    pop6.classList.remove("show");
+    pop7.classList.remove("show");
+    pop8.classList.remove("show");
+  }
